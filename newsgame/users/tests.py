e80c778f72b4aa50ed17c210c84fd71cc5709dc6
Christopher <christopher@seekingwebdev.com>
2020-02-13 12:14:43 +0000

    test views

--- newsgame/users/tests.py
+++ newsgame/users/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase, Client
 from rest_framework.test import APITestCase
 from users.models import CustomUser
+from django.urls import reverse, resolve
+from users.views import index, highscores, world, level01, level02, level03, SignUp
 
 class CustomUserCreateTestCase(APITestCase):
   def test_create_user(self):
@@ -14,6 +16,8 @@ class CustomUserCreateTestCase(APITestCase):
     }
     response = self.client.post('/user/', user_attrs)
     self.assertEqual(response.status_code, 201)
+    self.assertEqual(CustomUser.objects.count(), initial_user_count + 1)
+
   def test_update_user(self):
     initial_user_count = CustomUser.objects.count()
     user_attrs = {
@@ -33,3 +37,103 @@ class CustomUserCreateTestCase(APITestCase):
     }
     response = self.client.put('/user/1/', user_attrs_updated)
     self.assertEqual(response.status_code, 200)
+
+  def test_read_user(self):
+    initial_user_count = CustomUser.objects.count()
+    user_attrs = {
+      'username': 'Michael',
+      'points': 0,
+      'level': 1,
+      'email': 'michael@email.com',
+      'password': '1nsecure_passw0rd'
+    }
+    self.client.post('/user/', user_attrs)
+    get_user = self.client.get('/user/1/')
+    self.assertEqual(get_user.status_code, 200)
+
+  def test_delete_user(self):
+    initial_user_count = CustomUser.objects.count()
+    user_attrs = {
+      'username': 'Michael',
+      'points': 0,
+      'level': 1,
+      'email': 'michael@email.com',
+      'password': '1nsecure_passw0rd'
+    }
+    self.client.post('/user/', user_attrs)
+    get_user = self.client.get('/user/1/')
+    self.assertEqual(get_user.status_code, 200)
+    del_user = self.client.delete('/user/1/')
+    self.assertEqual(del_user.status_code, 204)
+    self.client.post('/user/', user_attrs)
+    get_user_again = self.client.get('/user/1/')
+    self.assertEqual(get_user_again.status_code, 404)
+  
+class TestUrls(SimpleTestCase):
+  def test_world_url(self):
+    url = reverse('world')
+    self.assertEquals(resolve(url).func, world)
+
+  def test_level1_url(self):
+    url = reverse('level01')
+    self.assertEquals(resolve(url).func, level01)
+    
+  def test_level2_url(self):
+    url = reverse('level02')
+    self.assertEquals(resolve(url).func, level02)
+    
+  def test_level3_url(self):
+    url = reverse('level03')
+    self.assertEquals(resolve(url).func, level03)
+    
+  def test_highscores_url(self):
+    url = reverse('highscores')
+    self.assertEquals(resolve(url).func, highscores)
+    
+  def test_signup_url(self):
+    url = reverse('signup')
+    self.assertEquals(resolve(url).func.view_class, SignUp)
+
+class TestViews(TestCase):
+  def test_index_view(self):
+    client = Client()
+    response = client.get(reverse('home'))
+    self.assertEquals(response.status_code, 200)
+    self.assertTemplateUsed(response, 'home.html')
+
+  def test_signup_view(self):
+    client = Client()
+    response = client.get(reverse('signup'))
+    self.assertEquals(response.status_code, 200)
+    self.assertTemplateUsed(response, 'signup.html')
+
+  def test_highscores_view(self):
+    client = Client()
+    response = client.get(reverse('highscores'))
+    self.assertEquals(response.status_code, 200)
+    self.assertTemplateUsed(response, 'highscores.html')
+
+  def test_world_view(self):
+    client = Client()
+    response = client.get(reverse('world'))
+    self.assertEquals(response.status_code, 200)
+    self.assertTemplateUsed(response, 'world.html')
+
+  def test_level1_view(self):
+    client = Client()
+    response = client.get(reverse('level01'))
+    self.assertEquals(response.status_code, 200)
+    self.assertTemplateUsed(response, 'level01.html')
+
+  def test_level2_view(self):
+    client = Client()
+    response = client.get(reverse('level02'))
+    self.assertEquals(response.status_code, 200)
+    self.assertTemplateUsed(response, 'level02.html')
+
+  def test_level3_view(self):
+    client = Client()
+    response = client.get(reverse('level03'))
+    self.assertEquals(response.status_code, 200)
+    self.assertTemplateUsed(response, 'level03.html')
+
